6c75ea21b0da2c00931ed3d686a05842c9c2d545
Joseph Kacvinsky <83467452+ReQuiem2099@users.noreply.github.com>
2025-07-09 04:10:20 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://joseph-kacvinsky.github.io',
+  site: 'https://kacvinsky.work/',
   base: '/'
 });
